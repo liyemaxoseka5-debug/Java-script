39569b408a1fb345c131f641710a3b2c823923d3
liyema xoseka <liyemaxoseka5@gmail.com>
2026-05-27 14:11:56 +0200

    add untracked files

---
+++ Exercise 6.js
@@ -0,0 +1,16 @@
+// TODO: Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
+// TODO: Create a function called add that takes two parameters
+// TODO: Make the add function return the sum of both parameters
+// TODO: Call both functions and store their results in variables
+// TODO: Print the results to the console
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+function add(a, b) {
+    return a + b;
+}
+let greetResult = greet("Matthew");
+let addResult = add(5, 3);
+console.log(greetResult);
+console.log(addResult);
