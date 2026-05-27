a0ca7288a8961f3e8f6d1ff08df3a715b271c231
Liyema Xoseka <liyemaxoseka5@gmail.com>
2026-05-27 14:54:37 +0200

    Add files via upload

---
+++ Exercise4.js
@@ -0,0 +1,24 @@
+//Liyema Xoseka and Busiswa Bala
+
+
+// TODO: Create a variable called score and give it a value between 0 and 100
+let score = 10;
+// TODO: Create a variable called passingGrade and set it to 60
+const passingGrade = 60;
+// TODO: Write an if statement to check if score is greater than or equal to passingGrade
+if (score >= passingGrade) {
+    console.log ("Passed");   
+}
+// TODO: Add an else statement for when the score is below passingGrade
+else { console.log ("Failed");
+}
+if (score == 100) {
+    console.log("You got a Perfect score!");
+}
+// TODO: Print "Passed" or "Failed" based on the score
+else {
+    console.log("This is not a perfect score.");   
+}
+// TODO: Add another condition to check if the score is perfect (100)
+score === 100 ? console.log("You got a Perfect score!!!"):
+console.log("This is not a perfect score.");
